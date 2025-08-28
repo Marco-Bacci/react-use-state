79477b08105fc5a0fa6ec64ae1599b1cc28f69e7
Marco Bacci <marcobacci94@gmail.com>
2025-08-28 17:54:22 +0200

    fix: cancel all components and do everything in Main

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,18 +1,26 @@
-
-import Languages from "./Languages";
-import Cards from "./Cards";
+import { useState } from "react";
+import languages from "../data/languages";
 const Main = () => {
+  const [currentInex, setCurrentIndex] = useState(0);
   return (
     <>
       <h1 className="mb-5">Learn Web development</h1>
       <div className="container">
-        <Languages></Languages>
-        <Cards></Cards>
+        {languages.map((language) => {
+          const { id, title } = language;
+          return (
+            <button key={id} type="button" className="btn btn-primary m-2">
+              {title}
+            </button>
+          );
+        })}
+        <div className="card mt-4 p-2">
+          <div className="title fw-bold">HTML</div>
+          <div className="description">description</div>
+        </div>
       </div>
     </>
   );
 };
 
 export default Main;
-   
-        
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Main from '../components/Main'
 import { useState } from 'react'
 function App() {
-  const [count, setCount] = useState(0)
-  // const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <Main />
-      
+
     </>
   )
 }
