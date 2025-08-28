e226f3abab4e1a9c57820cd7413ade0b5db8ef14
Marco Bacci <marcobacci94@gmail.com>
2025-08-29 01:43:13 +0200

    feat: add state var and put onClick on button

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import languages from "../data/languages";
 const Main = () => {
-  const [currentInex, setCurrentIndex] = useState(0);
+  const [selected, setSelected] = useState(languages[0]);
   return (
     <>
       <h1 className="mb-5">Learn Web development</h1>
@@ -9,14 +9,14 @@ const Main = () => {
         {languages.map((language) => {
           const { id, title } = language;
           return (
-            <button key={id} type="button" className="btn btn-primary m-2">
+            <button key={id} type="button" className="btn btn-primary m-2"onClick={()=> setSelected(language)} >
               {title}
             </button>
           );
         })}
         <div className="card mt-4 p-2">
-          <div className="title fw-bold">HTML</div>
-          <div className="description">description</div>
+          <div className="title fw-bold">{selected.title}</div>
+          <div className="description">{selected.description}</div>
         </div>
       </div>
     </>
