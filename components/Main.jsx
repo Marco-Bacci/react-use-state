8952c4197f98fe5a4114c968680e6620f926e6f9
Marco Bacci <marcobacci94@gmail.com>
2025-08-28 15:10:03 +0200

    Feat: create component languages from array and import into main

---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import Languages from "./Languages";
+const Main = () => {
+  return (
+    <>
+      <h1 className="mb-5">Learn Web development</h1>
+      <div className="container">
+        <Languages></Languages>
+   
+        <div className="card mt-4 p-2">
+          <div className="title fw-bold">HTML</div>
+          <div className="description">lorem ipsum docet</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Main from '../components/Main'
 import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Main />
     </>
   )
 }
